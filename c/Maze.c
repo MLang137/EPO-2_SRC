ec6c22a9631e14857275674a769380ccf3b6f0d1
Thijmen Batelaan <T.j.batelaan@student.tudelt.nl>
2022-05-04 19:02:18 +0200

    Changed Map array + algorithm beginning

--- c/Maze.c
+++ c/Maze.c
@@ -1,75 +1,65 @@
 #include <stdio.h>
 
-int maze[14][14];
+int maze[13][13];
 int startdirection;
-int * BaseToCords(int Base);
+int BaseToCords(int Base);
 void CreateMap(void);
-void MazeAlgorithm(int SourceNode, 
-int Target;
-
+void MazeAlgorithm(int Source,int Target);
 
 int main(){
 CreateMap();
-        for (int i = 1; i <= 13; i++){
-            for (int j = 1; j <= 13; j++){
-               printf( "%d\t",maze[i][j]);
+MazeAlgorithm(12,9);
+        for (int i = 0; i < 13; i++){
+            for (int j = 0; j < 13; j++){
+               printf( "  %d \t ",maze[i][j]);
             }
             printf("\n");
             printf("\n");
         }
+        
 }
 
-int* BaseToCords(int Base) {
+
+int BaseToCords(int Base) {
     int offset;
     // Changing Base numbers to actual map co-ordinates
     switch (Base) {
     case 1:
-        offset = 187;
-        startdirection = 1;
+        offset = 160;
+        
         break;
     case 2:
-        offset = 189;
-        startdirection = 1;
+        offset = 162;
         break;
     case 3:
-        offset = 191;
-        startdirection = 1;
+        offset = 164;
         break;
     case 4:
-        offset = 140;
-        startdirection = 4;
+        offset = 116;
         break;
     case 5:
-        offset = 112;
-        startdirection = 4;
+        offset = 103;
         break;
     case 6:
-        offset = 84;
-        startdirection = 4;
+        offset = 90;
         break;
     case 7:
-        offset = 24;
-        startdirection = 3;
+        offset = 8;
         break;
     case 8:
-        offset = 22;
-        startdirection = 3;
+        offset = 6;
         break;
     case 9:
-        offset = 20;
-        startdirection = 3;
+        offset = 4;
         break;
     case 10:
-        offset = 71;
-        startdirection = 2;
+        offset = 53;
         break;
     case 11:
-        offset = 99;
-        startdirection = 2;
+        offset = 66;
         break;
     case 12:
-        offset = 127;
-        startdirection = 2;
+        offset = 79;
         break;
 
     default:
@@ -78,100 +68,85 @@ int* BaseToCords(int Base) {
     return offset;
 }
 
-
-}
-void MazeAlgorithm(int Target, int Source){
+void MazeAlgorithm(int Source,int Target){
 int *MazeEdit = *maze;
-bool edge = true;
 int direction; 
+int pos;
 
 int TargetOffset = BaseToCords(Target);
 int StartOffset = BaseToCords(Source);
 
-*(MazeEdit + TargetOffset) = 1;
 int i = 1;
+*(MazeEdit + TargetOffset) = i++;
+
 
 pos = TargetOffset;
-int Yup = -14; // +y = offset - 14
-int Ydown = 14; // -y = offset + 14
-int Xup = 1; // +x = offset + 1
-int Xdown = -1;// -x = offset - 1
+printf("pos: %d \n", pos);
+printf("\n");
+int up = -13; // +y = offset - 14
+int dwn = 13; // -y = offset + 14
+int r = 1; // +x = offset + 1
+int l = -1;// -x = offset - 1
 
-if (startdirection = 1){
-    pos += Yup
-    *(MazeEdit + pos) = ++i;
-    pos += Yup
-    *(MazeEdit + pos) = ++i;  
-
-}
-if (startdirection = 2){
-    pos += Xdown
-    *(MazeEdit + pos) = ++i; 
-    pos += Xdown
-    *(MazeEdit + pos) = ++i; 
-}
-if (startdirection = 3){
-    pos += Ydown
-    *(MazeEdit + pos) = ++i; 
-    pos += Ydown
-    *(MazeEdit + pos) = ++i; 
-}
-if (startdirection == 4){
-    pos += Xup
-    *(MazeEdit + pos) = ++i; 
-        pos += Xup
-    *(MazeEdit + pos) = ++i; 
-}
-
-while(*(MazeEdit + StartOffset) == 0) {
-if(*(MazeEdit + TargetOffset + Yup) = 0 ){
-  
+for (int q; q < 40; q++){ {     // While target is not marked keep looking
+if( *(MazeEdit + TargetOffset + up) == 0 ){
+  pos += up;
+  *(MazeEdit + pos) = i++; 
+} else 
+if( *(MazeEdit + TargetOffset + dwn) == 0 ){
+  pos += dwn ;
+  *(MazeEdit + pos) = i++; 
+} else
+if( *(MazeEdit + TargetOffset + l) == 0 ){
+  pos += l;
+  *(MazeEdit + pos) = i++; 
+} else
+if( *(MazeEdit + TargetOffset + r) == 0 ){
+  pos += r;
+  *(MazeEdit + pos) = i++; 
 } 
 }
-
-
 }
-
-
-
+}
 void CreateMap(void){
 int i,j;
 // Creating Map Full of -1
-for (i = 1; i <= 13; i++){
-    for (j = 1; j <= 13; j++){
+for (i = 0; i < 13; i++){
+    for (j = 0; j < 13; j++){
         maze[i][j] = -1;
     }
 }
+
 // Creating horizontal Paths.
-for (i = 5; i <= 10 ; i++){
-    if (i == 5 || i == 7 || i == 9){
-        for (j = 0; j <= 13; j++){
+for (i = 4; i < 10 ; i++){
+    if (i == 4 || i == 6 || i == 8){
+        for (j = 0; j < 13; j++){
           maze[i][j] = 0;
         }
     }
 }
 
 // Creating vertical Paths.
-for (i = 5; i <= 10 ; i++){
-    if (i == 5 || i == 7 || i == 9){
-        for (j = 1; j <= 13; j++){
+for (i = 4; i < 9 ; i++){
+    if (i == 4 || i == 6 || i == 8){
+        for (j = 0; j < 13; j++){
           maze[j][i] = 0;
         }
     }
 }
 
 // Creating In between paths
-for (i = 3; i <= 11 ; i++){
-    if (i == 3 || i == 11){
-        for (j = 3; j <= 11; j++){
+for (i = 2; i < 11 ; i++){
+    if (i == 2 || i == 10){
+        for (j = 2; j < 11; j++){
           maze[j][i] = 0;
         }
     }
 }
 
-for (i = 3; i <= 11 ; i++){
-    if (i == 3 ||i == 11){
-        for (j = 3; j <= 11; j++){
+for (i = 2; i < 11 ; i++){
+    if (i == 2 ||i == 10){
+        for (j = 2; j < 11; j++){
           maze[i][j] = 0;
         }
     }
